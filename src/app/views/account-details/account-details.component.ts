5c20c6e3de613f993c2877a049d840c156c77e57
Jacky Lee <jackylee@Jackys-MBP.fios-router.home>
2017-10-22 19:41:21 -0700

    Added bar graph to display energy info

--- src/app/views/account-details/account-details.component.ts
+++ src/app/views/account-details/account-details.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
+import { Chart } from 'chart.js';
 
 import { AccountDetailService } from '../../services/account-detail.service';
 import { EnergyDaily, EnergySummary } from '../../types';
@@ -10,10 +12,15 @@ import { EnergyDaily, EnergySummary } from '../../types';
   styleUrls: ['./account-details.component.scss'],
   providers: [AccountDetailService]
 })
-export class AccountDetailsComponent implements OnInit {
+export class AccountDetailsComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('myBarChart') canvasRef: ElementRef;
+  @ViewChild('myChartAxis') canvas2Ref: ElementRef;
 
   public summary: EnergySummary;
   public dailies: EnergyDaily[];
+
+  private accountId: number;
 
   constructor(
     private AccountDetailService: AccountDetailService,
@@ -24,15 +31,84 @@ export class AccountDetailsComponent implements OnInit {
   }
 
   public ngOnInit() {
-    const accountId = this.ActivatedRoute.snapshot.params.accountId;
+    this.accountId = this.ActivatedRoute.snapshot.params.accountId;
     console.log("accountId", this.ActivatedRoute);
-    this.AccountDetailService.getSummary(accountId).subscribe(summary => {
+    this.AccountDetailService.getSummary(this.accountId).subscribe(summary => {
       this.summary = summary;
       console.log("summary", this.summary);
     });
-    this.AccountDetailService.getDailies(accountId).subscribe(dailies => {
+  }
+
+  public ngAfterViewInit() {
+    console.log(this.canvasRef);
+    this.AccountDetailService.getDailies(this.accountId).subscribe(dailies => {
       this.dailies = dailies.daily_energy_usage;
       console.log("dailies", this.dailies);
-    })
+      this.setupBarGraph(this.dailies);
+    });
   }
+
+  public setupBarGraph(data) {
+    var keys = [];
+    var values = [];
+    data.forEach(object => {
+      const key = Object.keys(object)[0];
+      keys.push(key.split("/")[1]);
+      values.push(object[key]);
+    });
+
+    let month = keys[0].split("/")[0];
+
+    let chartInstance = this.canvasRef.nativeElement;
+    let ctx = chartInstance.getContext("2d");
+    var chart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: keys,
+        datasets: [{
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: values,
+        }]
+      },
+
+      // Configuration options go here
+      options: {
+        animation: {
+          duration: 1,
+          onComplete: (animation) => {
+            let ctx = animation.chart.ctx;
+            let dataset = animation.chart.controller.getDatasetMeta(0);
+
+            ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'bottom';
+            // only one
+            dataset.data.forEach((bar, index) => {
+              ctx.fillText(values[index], bar._model.x, bar._model.y - 5);
+            });
+          }
+        },
+        hover: { animationDuration: 0 },
+        maintainAspectRatio: false,
+        layout: {
+          padding: {
+            left: 0,
+            right: 0,
+            top: 20,
+            bottom: 0
+          }
+        },
+        legend: { display: false },
+        responsiveness: false,
+        scales: {
+          yAxes: [{ gridLines: { drawBorder: false, display: true }, ticks: { display: false, beginAtZero: true} }],
+          xAxes: [{ gridLines: { display: false }, ticks: { display: true}}]
+        },
+        tooltips: { enabled: false }
+      }
+    });
+  }
+
+
 }
